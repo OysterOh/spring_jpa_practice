6cb81f63c42819d433058918233e26101b0ea538
jungwon <jungwon2433@naver.com>
2023-06-19 16:34:59 +0900

    # 6-1. 종합 연습예제 작성중(posts -> GET)

---
+++ src/main/java/com/spring/jpa/chap05_practice/dto/PostDetailResponseDTO.java
@@ -0,0 +1,35 @@
+package com.spring.jpa.chap05_practice.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.spring.jpa.chap05_practice.entity.Post;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter @Setter
+@ToString @EqualsAndHashCode
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class PostDetailResponseDTO {
+
+    private String author;
+    private String title;
+    private String content;
+    private List<String> hashTags;
+
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    private LocalDateTime regDate;
+
+    //엔티티를 DTO 로 변환하는 생성자
+    public PostDetailResponseDTO(Post post) {
+        this.author = post.getWriter();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.regDate = post.getCreateDate();
+
+        //HashTag 같은 경우에는 가공해서 집어넣어야 한다. (리스트의 타입이 서로 다르다.)
+        this.hashTags = post.getHashTags();
+
+    }
+}
